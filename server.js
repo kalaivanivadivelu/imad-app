3018546f1198a67a0aef2b625bb5884735ca58d7
kalaivanivadivelu <32031110+kalaivanivadivelu@users.noreply.github.com>
2017-09-18 00:04:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
   
-   var articals = {
+   var artical = {
    ' artical-one' : {
      title: 'artical-one kalai',
      heading: 'artical.one' ,
